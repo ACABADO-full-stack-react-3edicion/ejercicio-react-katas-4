19fa1d9af3986d6af0ecdfbc399b350ab2a8c7ec
Mario González <mario@mariogl.com>
2021-06-11 13:03:37 +0200

    Corrección

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [numeros, setNumeros] = useState([1, 2, 3, 4, 5]);
+  const [numeroSeleccionado, setNumeroSeleccionado] = useState(null);
+  const incrementa = () => {
+    if (numeros[numeros.length - 1] === 20) {
+      return;
+    }
+    setNumeros(numeros.map((numero) => numero + 1));
+  };
+  const decrementa = () => {
+    if (numeros[0] === 0) {
+      return;
+    }
+    setNumeros(numeros.map((numero) => numero - 1));
+  };
+  const pintaNumero = (numero) => {
+    setNumeroSeleccionado(numero);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul className="numeros">
+        <li onClick={decrementa}>{"<<"}</li>
+        {numeros.map((numero) => (
+          <li key={numero} onClick={() => pintaNumero(numero)}>
+            {numero}
+          </li>
+        ))}
+        <li onClick={incrementa}>{">>"}</li>
+      </ul>
+      <div className="numero">{numeroSeleccionado}</div>
+    </>
   );
 }
 
